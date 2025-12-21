56bc71a6725fae5c27577b6ed12dbfc6daffc6eb
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-22 00:26:56 +0530

    test(omit): add basic test for omit

---
+++ src/object/omit.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { omit } from "./omit";
+
+describe("omit", () => {
+  it("remove defined properties", () => {
+    const user = {
+      id: 1,
+      name: "Alice",
+      email: "alice@example.com",
+      age: 25,
+      password: "secret123",
+    };
+    const result = omit(user, ["password", "email"]);
+    expect(result).toStrictEqual({
+      id: 1,
+      name: "Alice",
+      age: 25,
+    });
+  });
+});
