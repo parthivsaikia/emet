9cfda2d00bbed28b44dbeca634c4dfa08da1473d
PARTHIV PRATIM SAIKIA <142080852+parthivsaikia@users.noreply.github.com>
2025-12-27 23:54:22 +0530

    docs(chunk): add ts-doc for chunk (#11)

--- src/array/chunk.ts
+++ src/array/chunk.ts
@@ -1,3 +1,18 @@
+/**
+ * Splits an array into sub arrays of length size
+ * @template T - The type of the elements of the array
+ * @param array - The source array
+ * @param size - The length of sub arrays
+ *
+ * @example
+ * ```
+ * const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ * const resultArr = chunk(arr, 4)
+ * console.log(resultArr)
+ * // [[1, 2, 3, 4], [5, 6, 7, 8], [9]]
+ * ```
+ */
+
 export function chunk<T>(array: T[], size: number): T[][] {
 	if (size <= 0) {
 		throw new Error(`size must be positive. Received size: ${size}`);
