9242d3d4b4f3535f50c7e24194700125a2c62114
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-27 17:50:28 +0530

    chore: fix linting according to biome

--- src/object/omit.test.ts
+++ src/object/omit.test.ts
@@ -1,20 +1,20 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import { omit } from "./omit";
 
 describe("omit", () => {
-  it("remove defined properties", () => {
-    const user = {
-      id: 1,
-      name: "Alice",
-      email: "alice@example.com",
-      age: 25,
-      password: "secret123",
-    };
-    const result = omit(user, ["password", "email"]);
-    expect(result).toStrictEqual({
-      id: 1,
-      name: "Alice",
-      age: 25,
-    });
-  });
+	it("remove defined properties", () => {
+		const user = {
+			id: 1,
+			name: "Alice",
+			email: "alice@example.com",
+			age: 25,
+			password: "secret123",
+		};
+		const result = omit(user, ["password", "email"]);
+		expect(result).toStrictEqual({
+			id: 1,
+			name: "Alice",
+			age: 25,
+		});
+	});
 });
