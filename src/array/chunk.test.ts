a1c044eee9800c5dce929fac641464a462338b57
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-27 21:06:31 +0530

    test(chunk): add complete test cases for chunk

--- src/array/chunk.test.ts
+++ src/array/chunk.test.ts
@@ -10,4 +10,48 @@ describe("chunk", () => {
 		const resultArr = chunk(array, size);
 		expect(resultArr).toEqual(expectedArr);
 	});
+	it("chunk an empty array", () => {
+		const array: number[] = [];
+		const size = 1;
+		const expectedArr: number[] = [];
+		const resultArr = chunk(array, size);
+		expect(resultArr).toEqual(expectedArr);
+	});
+	it("chunk with size 0", () => {
+		const array = [1, 2, 3];
+		const size = 0;
+		expect(() => {
+			chunk(array, size);
+		}).toThrowError(`size must be positive. Received size: ${size}`);
+	});
+	it("chunk with negative size", () => {
+		const array = [1, 2, 3];
+		const size = -3;
+		expect(() => {
+			chunk(array, size);
+		}).toThrowError(`size must be positive. Received size: ${size}`);
+	});
+	it("chunk with smaller array", () => {
+		const array = [1, 2];
+		const size = 5;
+		const expectedArr = [[1, 2]];
+		const resultArr = chunk(array, size);
+		expect(resultArr).toEqual(expectedArr);
+	});
+	it("array length is exactly divisible", () => {
+		const array = ["apple", "banana", "cat", "dog"];
+		const size = 2;
+		const expectedArr = [
+			["apple", "banana"],
+			["cat", "dog"],
+		];
+		const resultArr = chunk(array, size);
+		expect(resultArr).toEqual(expectedArr);
+	});
+	it("chunk doesn't mutate the original array", () => {
+		const array = [1, 2, 3];
+		const size = 2;
+		chunk(array, size);
+		expect(array).toEqual([1, 2, 3]);
+	});
 });
