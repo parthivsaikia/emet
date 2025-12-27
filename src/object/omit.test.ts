ab302964cca64eb5dcd0bd8fc14b214d577ffa8f
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-27 22:51:24 +0530

    test(omit): add tests for omit

--- src/object/omit.test.ts
+++ src/object/omit.test.ts
@@ -17,4 +17,20 @@ describe("omit", () => {
 			age: 25,
 		});
 	});
+	it("remove undefined property", () => {
+		const obj = { a: 1, b: 2, c: 3 };
+		// @ts-expect-error -- Testing using invalid key
+		const result = omit(obj, ["d"]);
+		expect(result).toStrictEqual(obj);
+	});
+	it("using empty keys", () => {
+		const obj = { a: 1, b: 2, c: 3 };
+		const result = omit(obj, []);
+		expect(result).toStrictEqual(obj);
+	});
+	it("omit function doesn't change original object", () => {
+		const obj = { a: 1, b: 2, c: 3 };
+		omit(obj, ["a"]);
+		expect(obj).toStrictEqual({ a: 1, b: 2, c: 3 });
+	});
 });
