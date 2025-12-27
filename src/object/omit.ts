8302901df3b82dff0fd56bccbd54f660945f650f
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-27 23:21:18 +0530

    docs(omit): added ts-doc for omit utility

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -1,6 +1,18 @@
 /**
+ * Remove specified properties from object
+ * @template T - The type of object
+ * @template K - The type of key to remove
+ * @param object - The source object
+ * @param keys - The array of keys to be omitted
  *
+ * @example
+ * ```
+ * const obj = {a: 1, b: 2, c: 3}
+ * const result = omit(obj, ["a", "b"])
+ * console.log(result)
  *
+ * // {c: 3}
+ * ```
  */
 
 export function omit<T extends object, K extends keyof T>(
