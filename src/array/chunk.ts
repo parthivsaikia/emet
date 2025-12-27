871e5c082a8170337741aea8a2d1955006a6bdf9
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-12-27 19:36:59 +0530

    refactor(chunk): use slice instead of for loop to improve performance

--- src/array/chunk.ts
+++ src/array/chunk.ts
@@ -1,12 +1,7 @@
 export function chunk<T>(array: T[], size: number): T[][] {
 	const returnArr = [];
-	let temp = [];
-	for (let i = 0; i < array.length; i++) {
-		temp.push(array[i]);
-		if (temp.length === size || i === array.length - 1) {
-			returnArr.push(temp);
-			temp = [];
-		}
+	for (let i = 0; i < array.length; i += size) {
+		returnArr.push(array.slice(i, i + size));
 	}
 	return returnArr;
 }
